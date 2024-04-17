7f7e16c68c60825c1b2f8d46353bca7462a2a1a5
nnguyenquangg <nguyenquang06012000@gmail.com>
2024-04-17 06:45:22 +0000

    feat: build dockerhub image step

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const os = require('os')
+// require('./tracer');
+const api = require('@opentelemetry/api')
 
 const app = express()
 app.get('/', (req, res) => {
+	console.log(`Hi ${os.hostname()}`)
 	res.send(`Hi ${os.hostname()}`)
 })
+
+// init('demo-node-service', 'development') // calling tracer with service name and environment to view in jaegerui
+
 
 app.listen(3000, () => console.log(`listen port 3000`));
